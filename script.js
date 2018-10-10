23a3f2399ef23421ccb11e673a04e16585fc5748
dekimioc <dekimioc@yahoo.com>
2018-10-10 20:13:35 +0200

    Function to show date

--- script.js
+++ script.js
@@ -10,3 +10,12 @@ var seconds = today.getSeconds();
 var showDate = document.getElementById("current-date").innerHTML = "Current date is: "+ day + "/" + month + "/" + year + "." + "Today is: " + daylist[day];
 var showTime = document.getElementById("current-time").innerHTML = "Current time is: " + hours + ":" + minutes + ":" + seconds;
 var showDateTime = document.getElementById("current-date-time").innerHTML =showDate + " / " + showTime;
+
+function displayDate() {
+    var x = document.getElementById("current-date");
+    if ( x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+     x.style.display = "none"
+    }
+ }
