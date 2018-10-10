20c10a8a15edcf4607821b9785aa46bb1cb2bbcf
dekimioc <dekimioc@yahoo.com>
2018-10-10 20:09:24 +0200

    Date values stored in variables

---
+++ script.js
@@ -0,0 +1,8 @@
+var today = new Date();
+var day = today.getDay();
+var daylist = ["Monday", "Tuesday", "Wendsday", "Thursday", "Friday", "Saturday", "Sunday"];
+var month = today.getMonth();
+var year = today.getFullYear();
+var hours = today.getHours();
+var minutes = today.getMinutes();
+var seconds = today.getSeconds();
