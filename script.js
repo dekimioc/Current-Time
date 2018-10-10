72c42a7bd482e240ba5061629c9ba67e9a291546
dekimioc <dekimioc@yahoo.com>
2018-10-10 20:11:31 +0200

    Selected elements to show date and time

--- script.js
+++ script.js
@@ -6,3 +6,7 @@ var year = today.getFullYear();
 var hours = today.getHours();
 var minutes = today.getMinutes();
 var seconds = today.getSeconds();
+
+var showDate = document.getElementById("current-date").innerHTML = "Current date is: "+ day + "/" + month + "/" + year + "." + "Today is: " + daylist[day];
+var showTime = document.getElementById("current-time").innerHTML = "Current time is: " + hours + ":" + minutes + ":" + seconds;
+var showDateTime = document.getElementById("current-date-time").innerHTML =showDate + " / " + showTime;
